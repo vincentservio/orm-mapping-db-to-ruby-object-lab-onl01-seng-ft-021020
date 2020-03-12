d8b7578ae0993dbf765ecbf9da096b120f808d09
Vincent Servio <vincentservio@gmail.com>
2020-03-12 06:59:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,42 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.new_from_db(row)
+      new_student = self.new  
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student
+  end
+  
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.all
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+  end
+end
+
+
+
+  
 end
